8966ead449d0d12c0e19b6952b77aa8c711bbd2f
Aditya Bisen <adityabisenn@gmail.com>
2024-03-18 02:36:13 +0530

    ratings added to the get request

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -5,7 +5,7 @@ from .models import FashionProduct
 class FashionProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = FashionProduct
-        fields = ['id', 'name', 'price', 'url', 'images', 'description', 'brand', 'colour', 'sizes', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'price', 'url', 'images', 'description', 'brand', 'colour', 'sizes', 'rating', 'num_reviews', 'gender', 'category_details', 'created_at', 'updated_at']
 
 class SwipePageDataSerializer(serializers.ModelSerializer):
     class Meta:
